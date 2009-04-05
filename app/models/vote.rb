e9db84a9a3bb393b672f1950736b1e679f783b9d
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 20:48:57 +1000

    Actually lets allow anonymous voting later when we have an implementation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,9 +3,7 @@ class Vote < ActiveRecord::Base
   belongs_to :message
   
   validates_presence_of :user_id, :message_id, :up
-  
-  # Allow nil user_id so that anonymous people can vote (will need to work on restricting this based on session)
-  validates_uniqueness_of :user_id, :scope => :message_id, :allow_nil => true
+  validates_uniqueness_of :user_id, :scope => :message_id#, :allow_nil => true
   
   def down?
     !up?
