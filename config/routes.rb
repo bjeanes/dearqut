bdf377f86e3b6134d19b8d19b1d27d9677e2cb39
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 20:52:03 +1000

    Removed SiteController and made the default route be the new message form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :votes
-
   map.resources :comments
-
   map.resources :messages
-
   map.resources :users
+  
   map.signup '/signup', :controller => "users", :action => "new"
   map.twitter_login '/login/twitter', :controller => "sessions", :action => "new", :twitter => true
-  map.root :controller => "site", :action => "index"
+  map.root :controller => "messages", :action => "new"
 end
