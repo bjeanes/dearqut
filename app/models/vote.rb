7234720f9df80c9a5f5b87b6214f42e214ee9833
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 20:48:05 +1000

    Allow nil user_ids on votes so that anonymous users can vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,9 @@ class Vote < ActiveRecord::Base
   belongs_to :message
   
   validates_presence_of :user_id, :message_id, :up
-  validates_uniqueness_of :user_id, :scope => :message_id
+  
+  # Allow nil user_id so that anonymous people can vote (will need to work on restricting this based on session)
+  validates_uniqueness_of :user_id, :scope => :message_id, :allow_nil => true
   
   def down?
     !up?
