a29207696a58692f0490da051a9ca107c47d6451
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 21:47:35 +1000

    No deleting/updating of messages (yet, anyway). Also the creator should ALWAYS be the current_user and, if not set, will mean the message is anonymous

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -32,16 +32,15 @@ class MessagesController < ApplicationController
     end
   end
 
-  # GET /messages/1/edit
-  def edit
-    @message = Message.find(params[:id])
-  end
-
   # POST /messages
   # POST /messages.xml
   def create
-    @message = Message.new(params[:message])
-
+    message = params[:message]
+    message.delete(:user_id)
+    
+    @message = Message.new(message)
+    @message.user = current_user
+  
     respond_to do |format|
       if @message.save
         flash[:notice] = 'Message was successfully created.'
@@ -54,32 +53,37 @@ class MessagesController < ApplicationController
     end
   end
 
-  # PUT /messages/1
-  # PUT /messages/1.xml
-  def update
-    @message = Message.find(params[:id])
-
-    respond_to do |format|
-      if @message.update_attributes(params[:message])
-        flash[:notice] = 'Message was successfully updated.'
-        format.html { redirect_to(@message) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @message.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1
-  # DELETE /messages/1.xml
-  def destroy
-    @message = Message.find(params[:id])
-    @message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(messages_url) }
-      format.xml  { head :ok }
-    end
-  end
+  # # GET /messages/1/edit
+  # def edit
+  #   @message = Message.find(params[:id])
+  # end
+  # 
+  # # PUT /messages/1
+  # # PUT /messages/1.xml
+  # def update
+  #   @message = Message.find(params[:id])
+  # 
+  #   respond_to do |format|
+  #     if @message.update_attributes(params[:message])
+  #       flash[:notice] = 'Message was successfully updated.'
+  #       format.html { redirect_to(@message) }
+  #       format.xml  { head :ok }
+  #     else
+  #       format.html { render :action => "edit" }
+  #       format.xml  { render :xml => @message.errors, :status => :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  # 
+  # # DELETE /messages/1
+  # # DELETE /messages/1.xml
+  # def destroy
+  #   @message = Message.find(params[:id])
+  #   @message.destroy
+  # 
+  #   respond_to do |format|
+  #     format.html { redirect_to(messages_url) }
+  #     format.xml  { head :ok }
+  #   end
+  # end
 end
