54bfe67516497b4ccf4f4d68a1a075f983489d7f
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 20:40:13 +1000

    Vote validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,11 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :message
+  
+  validates_presence_of :user_id, :message_id, :up
+  validates_uniqueness_of :user_id, :scope => :message_id
+  
+  def down?
+    !up?
+  end
 end
