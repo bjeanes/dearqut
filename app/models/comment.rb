929ec1eb9a0b3af71153ed2eaedc39fd9ef6a205
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 20:44:53 +1000

    Validations and method to display author for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :message
+  
+  validates_presence_of :body, :message_id
+  
+  def author
+    (user || "Anonymous").to_s
+  end
 end
