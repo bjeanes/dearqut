67d2da6c4b226fc507bf245c75f857af2e017058
Bodaniel Jeanes <me@bjeanes.com>
2009-04-05 20:48:00 +1000

    Added sessions

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,39 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090405085838) do
+ActiveRecord::Schema.define(:version => 20090405104746) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.integer  "message_id", :null => false
+    t.text     "body",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["message_id"], :name => "index_comments_on_message_id"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "messages", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.text     "body",       :null => false
+    t.boolean  "twitter",    :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["twitter"], :name => "index_messages_on_twitter"
+  add_index "messages", ["user_id"], :name => "index_messages_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login"
@@ -32,5 +64,17 @@ ActiveRecord::Schema.define(:version => 20090405085838) do
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.integer  "message_id", :null => false
+    t.boolean  "up",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["message_id"], :name => "index_votes_on_message_id"
+  add_index "votes", ["user_id", "message_id"], :name => "index_votes_on_user_id_and_message_id", :unique => true
+  add_index "votes", ["user_id"], :name => "index_votes_on_user_id"
 
 end
