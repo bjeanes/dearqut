440b987c4b8c268a24dc3ba28a6bc6239056887e
Bodaniel Jeanes <me@bjeanes.com>
2009-04-19 19:39:58 +1000

    Set table name for User model cleaner

--- app/bot/dearqut/models.rb
+++ app/bot/dearqut/models.rb
@@ -4,16 +4,13 @@ unless defined? TwitterAuth::GenericUser
   module TwitterAuth
     class GenericUser < ActiveRecord::Base
       # Hack to get the User model to load
+      def self.table_name; 'users' end
     end
   end
 end
 
 Dir[File.join(RAILS_ROOT, "app", "models", "*.rb")].each do |model|
   require model
-end
-
-class User < TwitterAuth::GenericUser
-  set_table_name :users
 end
 
 config = YAML.load_file(File.join(RAILS_ROOT, "config", "database.yml"))[RAILS_ENV]
