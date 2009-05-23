00d9c88ac67d21eeb178b859c3f73e774c74b9b6
Bodaniel Jeanes <me@bjeanes.com>
2009-05-23 21:06:10 +1000

    Removed Prototype, added JQuery and created unobtrusive functions for voting up and down (functional)

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,24 @@
 class VotesController < ApplicationController
+  before_filter :build_vote
   
+  def up
+    @vote.value = 1
+    save_vote
+  end
+  
+  def down
+    @vote.value = -1
+    save_vote
+  end
+  
+  protected
+  
+    def build_vote
+      message = Message.find(params[:message_id])
+      @vote = message.votes.build(:user => current_user)
+    end
+    
+    def save_vote
+      render :text => (@vote.save ? "OK" : "Damn")
+    end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  $('.controls .agree a').click(agree);
+  $('.controls .disagree a').click(disagree);
+});
+
+function agree() {
+  vote(this, 'up');
+}
+
+function disagree() {
+  vote(this, 'down');
+}
+
+function getMessageId(vote) {
+  id = $(vote).parent().parent()[0].id;
+  return (+id.split('-')[1]);
+}
+  
+
+function vote(message, direction) {
+  if (typeof value != 'number')
+    message = getMessageId(message);
+  
+  var url = "/messages/" + message.toString() + "/vote/" + direction;
+  
+  $.post(url, {}, function(data) {
+    console.log(data.toString());
+  }, 'text');
+}
