8b72fe79d86da468c101d6644a5ad6a710966757
Bodaniel Jeanes <me@bjeanes.com>
2009-05-23 19:48:15 +1000

    Cleaned up routes and started voting features

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,10 +2,10 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :message
   
-  validates_presence_of :user_id, :message_id, :up
-  validates_uniqueness_of :user_id, :scope => :message_id#, :allow_nil => true
-  
-  def down?
-    !up?
-  end
+  validates_presence_of   :user_id, :message_id, :value
+  validates_uniqueness_of :user_id, :scope => :message_id #, :allow_blank => true
+  validates_inclusion_of  :value, :in => [-1, 1]
+
+  named_scope :positive, :conditions => "value > 0"
+  named_scope :negative, :conditions => "value < 0"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :comments
-
-  map.resources :messages
-
-  map.resources :votes
-
-  map.resources :votes
-  map.resources :comments
-  map.resources :messages
   map.resources :users
+  
+  map.resources :messages do |mes|
+    mes.resources :comments
+    mes.resource  :vote, :member => {:up => :post, :down => :post}, :only => []
+  end
   
   map.signup '/signup', :controller => "users", :action => "new"
   map.twitter_login '/login/twitter', :controller => "sessions", :action => "new", :twitter => true
+  
   map.root :controller => "messages", :action => "new"
 end
