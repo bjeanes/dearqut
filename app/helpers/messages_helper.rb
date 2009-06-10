4bdc25fd522c7e2f811a72702b3e50f9d40f172b
Bodaniel Jeanes <me@bjeanes.com>
2009-06-11 00:51:57 +1000

    Show time in message edit

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -4,4 +4,13 @@ module MessagesHelper
     sentence = '<em>none</em>' if sentence.blank?
     sentence
   end
+  
+  def message_time(message)
+    if Time.now - @message.created_at < 5.minutes
+      Time.now.to_s(:short)
+    else
+      "<del>#{@message.created_at.to_s(:short)}</del>
+       <ins>#{Time.now.to_s(:short)}</ins>"
+    end
+  end
 end
