e4d74fe81b1375e5e0cb9ab4add68957830a0ea1
Bodaniel Jeanes <me@bjeanes.com>
2009-06-11 00:04:03 +1000

    Show "NONE" in italics if a message has no tags

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,7 @@
 module MessagesHelper
+  def tag_sentence(tags)
+    sentence = tags.map { |tag| link_to_tag(tag) }.to_sentence.strip
+    sentence = '<em>none</em>' if sentence.blank?
+    sentence
+  end
 end
