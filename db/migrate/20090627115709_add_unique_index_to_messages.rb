69c19b0749db7cc4cd0400d53ab2f151d941a9bc
Bodaniel Jeanes <me@bjeanes.com>
2009-06-27 22:12:50 +1000

    Fix migration

--- db/migrate/20090627115709_add_unique_index_to_messages.rb
+++ db/migrate/20090627115709_add_unique_index_to_messages.rb
@@ -1,9 +1,11 @@
 class AddUniqueIndexToMessages < ActiveRecord::Migration
   def self.up
+    remove_index :messages, :tweet_id
     add_index :messages, :tweet_id, :unique => true
   end
 
   def self.down
     remove_index :messages, :tweet_id
+    add_index :messages, :tweet_id
   end
 end
