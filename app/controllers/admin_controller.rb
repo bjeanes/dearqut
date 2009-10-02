2e182ec0af2b657721e48d0c19685b0424cd52bc
Bodaniel Jeanes <me@bjeanes.com>
2009-10-02 23:08:11 +1000

    Remove index from admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,12 +2,7 @@ class AdminController < ApplicationController
   before_filter :admin_required
   layout 'admin'
   
-  def index
-    
-  end
-  
   private
-  
   def admin_required
     unless current_user && current_user.admin?
       session[:return_to_admin] = true
