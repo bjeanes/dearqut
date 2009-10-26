f1c55998f8823a39454a60ab6e9c82333fbdf6fe
Ri Liu <lilacparrot@gmail.com>
2009-10-26 10:44:33 +1000

    created comments styles fix

--- app/controllers/my/messages_controller.rb
+++ app/controllers/my/messages_controller.rb
@@ -2,8 +2,8 @@ module My
   class MessagesController < BaseController
 
     def index
-      @messages = current_user.messages.paginate(:order => "updated_at", :page => params[:page])
-      @grouped_messages = @messages.group_by {|m| m.updated_at.strftime("%Y-%m-%d")}
+      @messages = current_user.messages.paginate(:order => "created_at DESC", :page => params[:page])
+      @grouped_messages = @messages.group_by {|m| m.updated_at.strftime("%d-%m-%Y")}
     end
 
   end
