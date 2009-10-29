f2b076cb5aa29a8ae0055cceb0a21538cc788df5
Ri Liu <lilacparrot@gmail.com>
2009-10-29 16:50:25 +1000

    tidied my activities

--- app/controllers/my/activity_controller.rb
+++ app/controllers/my/activity_controller.rb
@@ -2,6 +2,7 @@ module My
   class ActivityController < BaseController
     def index
       @activities = current_user.activities.paginate(:page => params[:page])
+      @grouped_activities = @activities.group_by {|m| m.created_at.strftime("%d-%m-%Y")}
     end
 
   end
